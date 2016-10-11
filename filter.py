0a17eee4e41a1e440af6977dbf99ad4dd78febe6
xuertowong <xuertowong@gmail.com>
2016-10-11 18:17:15 +0800

    filter.py

--- filter.py
+++ filter.py
@@ -3,13 +3,12 @@ from pandas import DataFrame, Series
 import os,sys
 import re
 
-def select(address,parttern1,pattern2):
+def select(address,pattern2):
     list1 = os.listdir(address)
     list1 = Series(list1)
-    c = list1.str.contains(parttern1)
-    for i in range(len(c)):
-        if c[i] == True:
-            data = pd.read_csv(address+list1[i])
-            data = data[data[data.columns[0]].str.contains(pattern2]
-            data.to_csv(address+'1'+list1[i])
+    for i in range(len(list1)):
+        data = pd.read_csv(address+list1[i])
+        data = data.dropna()
+        data = data[data['gene'].str.contains(pattern2)]
+        data.to_csv(address+'1'+list1[i])
             
